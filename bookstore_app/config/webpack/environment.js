c121b8725a54516d2ddbd966be8015752c17d124
b6018851 <b6018851@my.shu.ac.uk>
2020-01-20 15:19:42 +0000

    Create Rails App and Configured Bootstrap

--- bookstore_app/config/webpack/environment.js
+++ bookstore_app/config/webpack/environment.js
@@ -1,3 +1,9 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+	$: 'jquery',
+	jquery: 'jquery',
+	Popper: ['popper.js', 'default'}
+	}))
 
 module.exports = environment
